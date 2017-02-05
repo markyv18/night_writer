027bedcb0f4e799497f8bba9d7569c8e27121467
Danny Radden <dradden@gmail.com>
2017-02-05 13:35:58 -0700

    clean up

--- lib/night_read.rb
+++ lib/night_read.rb
@@ -1,7 +1,13 @@
 require_relative './read_in_braille'
-require 'pry'
+# Imports braille file and writes conversion to Message.txt
 class NightRead
-  attr_reader :text_output, :braille_to_convert, :braille_array, :braille_sentence, :braille_letter, :text_sentence_array, :text_sentence
+  attr_reader :text_output,
+              :braille_to_convert,
+              :braille_array,
+              :braille_sentence,
+              :braille_letter,
+              :text_sentence_array,
+              :text_sentence
 
   def initialize
     read = ReadBraille.new(ARGV[0])
@@ -10,7 +16,11 @@ class NightRead
   end
 
   def convert_to_array
-    @braille_array = braille_to_convert.split(/\n+/)
+    @braille_array = braille_to_convert.split(/\n/)
+    reduce_to_one_line
+  end
+
+  def reduce_to_one_line
     until braille_array[3].nil?
       braille_array[0] += braille_array.delete_at(3)
       braille_array[1] += braille_array.delete_at(3)
@@ -40,24 +50,43 @@ class NightRead
   end
 
   def capitalize_and_numberize
-    text_sentence.gsub!(/\^./) { |letter| letter.delete("^").upcase }
+    text_sentence.gsub!(/\^./) { |letter| letter.delete('^').upcase }
     text_sentence.gsub!(/\#./) { |letter| numbers[letter] }
     text_sentence
   end
 
   def alphabet
-    @alphabet = {"a" => ["0.","..",".."], "b" => ["0.","0.",".."], "c" => ["00","..",".."], "d" => ["00",".0",".."], "e" => ["0.",".0",".."], "f" => ["00","0.",".."], "g" => ["00","00",".."], "h" => ["0.","00",".."], "i" => [".0","0.",".."], "j" => [".0","00",".."], "k" => ["0.","..","0."], "l" => ["0.","0.","0."], "m" => ["00","..","0."], "n" => ["00",".0","0."], "o" => ["0.",".0","0."], "p" => ["00","0.","0."], "q" => ["00","00","0."], "r" => ["0.","00","0."], "s" => [".0","0.","0."], "t" => [".0","00","0."], "u" => ["0.","..","00"], "v" => ["0.","0.","00"], "w" => [".0","00",".0"], "x" => ["00","..","00"], "y" => ["00",".0","00"], "z" => ["0.",".0","00"], "!" => ["..","00","0."], "'" => ["..","..","0."], "," => ["..","0.",".."], "-" => ["..","..","00"], "." => ["..","00",".0"], "?" => ["..","0.","00"], "^" => ["..", "..", ".0"], "#" => [".0", ".0", "00"], " " => ["..","..",".."]}
+    @alphabet = { 'a' => ['0.', '..', '..'], 'b' => ['0.', '0.', '..'],
+                  'c' => ['00', '..', '..'], 'd' => ['00', '.0', '..'],
+                  'e' => ['0.', '.0', '..'], 'f' => ['00', '0.', '..'],
+                  'g' => ['00', '00', '..'], 'h' => ['0.', '00', '..'],
+                  'i' => ['.0', '0.', '..'], 'j' => ['.0', '00', '..'],
+                  'k' => ['0.', '..', '0.'], 'l' => ['0.', '0.', '0.'],
+                  'm' => ['00', '..', '0.'], 'n' => ['00', '.0', '0.'],
+                  'o' => ['0.', '.0', '0.'], 'p' => ['00', '0.', '0.'],
+                  'q' => ['00', '00', '0.'], 'r' => ['0.', '00', '0.'],
+                  's' => ['.0', '0.', '0.'], 't' => ['.0', '00', '0.'],
+                  'u' => ['0.', '..', '00'], 'v' => ['0.', '0.', '00'],
+                  'w' => ['.0', '00', '.0'], 'x' => ['00', '..', '00'],
+                  'y' => ['00', '.0', '00'], 'z' => ['0.', '.0', '00'],
+                  '!' => ['..', '00', '0.'], "'" => ['..', '..', '0.'],
+                  ',' => ['..', '0.', '..'], '-' => ['..', '..', '00'],
+                  '.' => ['..', '00', '.0'], '?' => ['..', '0.', '00'],
+                  '^' => ['..', '..', '.0'], '#' => ['.0', '.0', '00'],
+                  ' ' => ['..', '..', '..'] }
   end
 
   def numbers
-    @number =    { "#j" => "0", "#a" => "1", "#b" => "2", "#c" => "3", "#d" => "4", "#e" => "5", "#f" => "6", "#g" => "7", "#h" => "8", "#i" => "9"}
+    @number = { '#j' => '0', '#a' => '1', '#b' => '2', '#c' => '3',
+                '#d' => '4', '#e' => '5', '#f' => '6', '#g' => '7',
+                '#h' => '8', '#i' => '9' }
   end
 end
 
 night = NightRead.new
 
 puts night.braille_to_convert
-p night.convert_to_array
-p night.make_lettered_array
-p night.convert_to_text
+night.convert_to_array
+night.make_lettered_array
+night.convert_to_text
 p night.capitalize_and_numberize
