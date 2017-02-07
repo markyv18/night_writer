e0eb6b580f9d369fa85e855b0e70cb50ca400b7b
Danny Radden <dradden@gmail.com>
2017-02-07 13:09:13 -0700

    added a dictionary to pull alphabet and numbers from

--- lib/night_read.rb
+++ lib/night_read.rb
@@ -1,4 +1,5 @@
 require './lib/read_in_braille'
+require './lib/dictionary'
 # Imports braille file and writes conversion to Message.txt
 class NightRead
   attr_reader :text_output,
@@ -7,11 +8,16 @@ class NightRead
               :braille_sentence,
               :braille_letter,
               :text_array,
-              :text_sentence
+              :text_sentence,
+              :alphabet,
+              :numbers
 
   def initialize
     read = ReadBraille.new(ARGV[0])
     @braille_to_convert = read.imported_braille
+    dictionary = Dictionary.new
+    @alphabet = dictionary.alphabet
+    @numbers = dictionary.numbers
     @braille_sentence = []
   end
 
@@ -59,35 +65,7 @@ class NightRead
     File.write('./data/' + ARGV[1], text_sentence)
   end
 
-  def alphabet
-    @alphabet = { 'a' => ['0.', '..', '..'], 'b' => ['0.', '0.', '..'],
-                  'c' => ['00', '..', '..'], 'd' => ['00', '.0', '..'],
-                  'e' => ['0.', '.0', '..'], 'f' => ['00', '0.', '..'],
-                  'g' => ['00', '00', '..'], 'h' => ['0.', '00', '..'],
-                  'i' => ['.0', '0.', '..'], 'j' => ['.0', '00', '..'],
-                  'k' => ['0.', '..', '0.'], 'l' => ['0.', '0.', '0.'],
-                  'm' => ['00', '..', '0.'], 'n' => ['00', '.0', '0.'],
-                  'o' => ['0.', '.0', '0.'], 'p' => ['00', '0.', '0.'],
-                  'q' => ['00', '00', '0.'], 'r' => ['0.', '00', '0.'],
-                  's' => ['.0', '0.', '0.'], 't' => ['.0', '00', '0.'],
-                  'u' => ['0.', '..', '00'], 'v' => ['0.', '0.', '00'],
-                  'w' => ['.0', '00', '.0'], 'x' => ['00', '..', '00'],
-                  'y' => ['00', '.0', '00'], 'z' => ['0.', '.0', '00'],
-                  '!' => ['..', '00', '0.'], "'" => ['..', '..', '0.'],
-                  ',' => ['..', '0.', '..'], '-' => ['..', '..', '00'],
-                  '.' => ['..', '00', '.0'], '?' => ['..', '0.', '00'],
-                  '^' => ['..', '..', '.0'], '#' => ['.0', '.0', '00'],
-                  ' ' => ['..', '..', '..'], 'and' => ['00', '0.', '00'],
-                  'for' => ['00', '00', '00'], 'of' => ['0.', '00', '00'],
-                  'the' => ['.0', '0.', '00'], 'with' => ['.0', '00', '00'],
-                  'ch' => ['0.', '..', '.0'] }
-  end
 
-  def numbers
-    @number = { '#j' => '0', '#a' => '1', '#b' => '2', '#c' => '3',
-                '#d' => '4', '#e' => '5', '#f' => '6', '#g' => '7',
-                '#h' => '8', '#i' => '9' }
-  end
 end
 
 night = NightRead.new
